97f820e8da2b49e240e3ec4af699c1b1e9d320a9
Juan Carlos Fernández <juan.carlos.fernandez@ricoh.es>
2025-01-31 13:53:48 +0100

    added items data + tailwind styles

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,16 @@
+import type { MenuItemT } from '../types';
 
-export default function MenuItem() {
+interface MenuItemProps {
+  item: MenuItemT;
+}
+
+export default function MenuItem( { item } : MenuItemProps ) {
   return (
-    <div>MenuItems</div>
+    <button
+      className="border-2 border-teal-400 p-3 w-full flex justify-between hover:bg-teal-200 hover:text-white"
+    >
+      <p>{item.name}</p>
+      <p className="font-black">{item.price} €</p>
+    </button>
   )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export type MenuItem = {
+export type MenuItemT = {
   id: number;
   name: string;
   price: number;
