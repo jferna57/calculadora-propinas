f8effc658c34bbe0d721c0f32b95e441e9b221f2
Juan Carlos Fernández <juan.carlos.fernandez@ricoh.es>
2025-01-31 13:30:05 +0100

    added list of menuitems

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
-import './index.css';
+import MenuItem from './components/MenuItem';
 import { menuItems } from './data/db';
+import './index.css';
 
 function App() {
-
-  console.log('MenuItems ', menuItems)
 
   return (
     <>
       <header className= " bg-teal-400 py-5">
         <h1 className=" text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
       </header>
+
+      <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+          
+          {menuItems.map(item  => ((
+            <MenuItem 
+               />
+          )))}
+          
+        </div>
+        
+        <div>
+          <h2>Consumo</h2>
+        </div>
+        
+      </main>
+
     </>
   );
 }
